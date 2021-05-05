050cf539f9f54d2a796fcc3e5fbeeb9d51b9e041
Bramanda Suwandi <bramanda48@gmail.com>
2021-05-05 09:53:58 +0700

    Comment @UsePipes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,19 +12,19 @@ export class AppController {
     private readonly logger = new Logger(AppController.name);
 
 	@GrpcMethod('Session', 'Create')
-    @UsePipes(new ValidationPipe({
-        exceptionFactory: (error: ValidationError[] = []) => {
-            let errorMessage: string[] = [];
-            error[0].children.forEach(function(v) {
-                const message: string[] = Object.values(v.constraints);
-                errorMessage.push(...message);
-            });
-            return new RpcException({
-                code: GrpcStatus.INVALID_ARGUMENT,
-                message: errorMessage.join(','),
-            });
-        },
-    }))
+    // @UsePipes(new ValidationPipe({
+    //     exceptionFactory: (error: ValidationError[] = []) => {
+    //         let errorMessage: string[] = [];
+    //         error[0].children.forEach(function(v) {
+    //             const message: string[] = Object.values(v.constraints);
+    //             errorMessage.push(...message);
+    //         });
+    //         return new RpcException({
+    //             code: GrpcStatus.INVALID_ARGUMENT,
+    //             message: errorMessage.join(','),
+    //         });
+    //     },
+    // }))
 	create(@Body() data: CreateSessionDto, metadata: Metadata): CreateSessionResponseDto {
         this.logger.debug(JSON.stringify(data));
 		return new CreateSessionResponseDto('This is response');
